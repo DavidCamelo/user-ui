2a3e4039ca84ad1a11110a1517ec3525fea659a0
David Camelo <dacaro16@gmail.com>
2025-06-06 18:15:26 -0500

    Setup User UI

--- src/user/User.jsx
+++ src/user/User.jsx
@@ -1,13 +1,13 @@
-import { useState } from "react";
-import Input from "components_ui/Input";
-import List from "components_ui/List";
+import { useState } from "react"
+import { InputTest } from "components_ui/InputTest"
+import { List } from "components_ui/List"
 
 function User() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Input value={count} onChange={setCount} onSubmit={() => setCount((count) => count + 1)} />
+      <InputTest value={count} onChange={setCount} onSubmit={() => setCount((count) => count + 1)} />
       <List items={["User UI", "Learn Vite", "Make an awesome app"]} />
     </>
   )
